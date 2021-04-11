bc5a3498a3661446f54c1f51a0195b71354ce2b8
Miro Knejp <miro.knejp@gmail.com>
2021-04-11 19:52:51 +0200

    Extract GeneratorTools tests into separate class

---
+++ src/dotVariant.Generator.Test/GeneratorTools.Test.cs
@@ -0,0 +1,77 @@
+//
+// Copyright Miro Knejp 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//
+
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using static dotVariant.Generator.Test.GeneratorTools;
+
+namespace dotVariant.Generator.Test
+{
+    internal static class GeneratorTools_Test
+    {
+        [TestCaseSource(nameof(ExtractExpectationsTest_cases))]
+        public static void ExtractExpectationsTest(string input, DiagnosticExpectation[] expected)
+        {
+            Assert.That(ExtractExpectations(input), Is.EquivalentTo(expected));
+        }
+
+        public static IEnumerable<TestCaseData> ExtractExpectationsTest_cases()
+        {
+            return
+                new (string Input, DiagnosticExpectation[] Expectations)[]
+                {
+                    (
+                        "// expected-error Foo",
+                        new DiagnosticExpectation[]
+                        {
+                            new(1, -1, "Foo", DiagnosticSeverity.Error),
+                        }
+                    ),
+                    (
+                        "// expected-warning Foo",
+                        new DiagnosticExpectation[]
+                        {
+                            new(1, -1, "Foo", DiagnosticSeverity.Warning),
+                        }
+                    ),
+                    (
+                        "// expected-error:51 Foo",
+                        new DiagnosticExpectation[]
+                        {
+                            new(1, 51, "Foo", DiagnosticSeverity.Error),
+                        }
+                    ),
+                    (
+                        @"line 1
+                        line 2
+                        line 3 // expected-error Foo",
+                        new DiagnosticExpectation[]
+                        {
+                            new(3, -1, "Foo", DiagnosticSeverity.Error),
+                        }
+                    ),
+                    (
+                        @"line 1
+                        line 2 // expected-error Foo
+                        line 3
+                        line 4 // expected-error:14 Bar
+                        line 5",
+                        new DiagnosticExpectation[]
+                        {
+                            new(2, -1, "Foo", DiagnosticSeverity.Error),
+                            new(4, 14, "Bar", DiagnosticSeverity.Error),
+                        }
+                    ),
+                }
+                .Select((test, i) =>
+                    new TestCaseData(test.Input, test.Expectations)
+                    .SetName($"{nameof(ExtractExpectationsTest)}({i})"));
+        }
+
+    }
+}
