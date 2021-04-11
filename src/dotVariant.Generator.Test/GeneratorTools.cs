05f9da413ade63d18bf8a2c91715c2d48cd19dc2
Miro Knejp <miro.knejp@gmail.com>
2021-04-11 19:58:11 +0200

    Extract GeneratorTools tests into separate class (#12)

--- src/dotVariant.Generator.Test/GeneratorTools.cs
+++ src/dotVariant.Generator.Test/GeneratorTools.cs
@@ -103,65 +103,6 @@ namespace dotVariant.Generator.Test
                     Severity: m.Groups[1].Value == "error" ? DiagnosticSeverity.Error : DiagnosticSeverity.Warning);
         }
 
-        [TestCaseSource(nameof(ExtractExpectationsTest_cases))]
-        public static void ExtractExpectationsTest(string input, DiagnosticExpectation[] expected)
-        {
-            Assert.That(ExtractExpectations(input), Is.EquivalentTo(expected));
-        }
-
-        public static IEnumerable<TestCaseData> ExtractExpectationsTest_cases()
-        {
-            return
-                new (string Input, DiagnosticExpectation[] Expectations)[]
-                {
-                    (
-                        "// expected-error Foo",
-                        new DiagnosticExpectation[]
-                        {
-                            new(1, -1, "Foo", DiagnosticSeverity.Error),
-                        }
-                    ),
-                    (
-                        "// expected-warning Foo",
-                        new DiagnosticExpectation[]
-                        {
-                            new(1, -1, "Foo", DiagnosticSeverity.Warning),
-                        }
-                    ),
-                    (
-                        "// expected-error:51 Foo",
-                        new DiagnosticExpectation[]
-                        {
-                            new(1, 51, "Foo", DiagnosticSeverity.Error),
-                        }
-                    ),
-                    (
-                        @"line 1
-                        line 2
-                        line 3 // expected-error Foo",
-                        new DiagnosticExpectation[]
-                        {
-                            new(3, -1, "Foo", DiagnosticSeverity.Error),
-                        }
-                    ),
-                    (
-                        @"line 1
-                        line 2 // expected-error Foo
-                        line 3
-                        line 4 // expected-error:14 Bar
-                        line 5",
-                        new DiagnosticExpectation[]
-                        {
-                            new(2, -1, "Foo", DiagnosticSeverity.Error),
-                            new(4, 14, "Bar", DiagnosticSeverity.Error),
-                        }
-                    ),
-                }
-                .Select((test, i) =>
-                    new TestCaseData(test.Input, test.Expectations)
-                    .SetName($"{nameof(ExtractExpectationsTest)}({i})"));
-        }
-
         public static readonly ImmutableDictionary<string, string> SupportSources =
             new Dictionary<string, string>()
                 {
