9140b1cbe02b2845f39ba0af0d6b9e660050565f
Miro Knejp <miro.knejp@gmail.com>
2021-11-11 05:59:36 +0100

    Add test to ensure the 255 option limit is diagnosed

---
+++ src/dotVariant.Generator.Test/diagnostics/NotTooManyOptions.cs
@@ -0,0 +1,785 @@
+//
+// Copyright Miro Knejp 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//
+
+internal enum E1 { }
+internal enum E2 { }
+internal enum E3 { }
+internal enum E4 { }
+internal enum E5 { }
+internal enum E6 { }
+internal enum E7 { }
+internal enum E8 { }
+internal enum E9 { }
+internal enum E10 { }
+internal enum E11 { }
+internal enum E12 { }
+internal enum E13 { }
+internal enum E14 { }
+internal enum E15 { }
+internal enum E16 { }
+internal enum E17 { }
+internal enum E18 { }
+internal enum E19 { }
+internal enum E20 { }
+internal enum E21 { }
+internal enum E22 { }
+internal enum E23 { }
+internal enum E24 { }
+internal enum E25 { }
+internal enum E26 { }
+internal enum E27 { }
+internal enum E28 { }
+internal enum E29 { }
+internal enum E30 { }
+internal enum E31 { }
+internal enum E32 { }
+internal enum E33 { }
+internal enum E34 { }
+internal enum E35 { }
+internal enum E36 { }
+internal enum E37 { }
+internal enum E38 { }
+internal enum E39 { }
+internal enum E40 { }
+internal enum E41 { }
+internal enum E42 { }
+internal enum E43 { }
+internal enum E44 { }
+internal enum E45 { }
+internal enum E46 { }
+internal enum E47 { }
+internal enum E48 { }
+internal enum E49 { }
+internal enum E50 { }
+internal enum E51 { }
+internal enum E52 { }
+internal enum E53 { }
+internal enum E54 { }
+internal enum E55 { }
+internal enum E56 { }
+internal enum E57 { }
+internal enum E58 { }
+internal enum E59 { }
+internal enum E60 { }
+internal enum E61 { }
+internal enum E62 { }
+internal enum E63 { }
+internal enum E64 { }
+internal enum E65 { }
+internal enum E66 { }
+internal enum E67 { }
+internal enum E68 { }
+internal enum E69 { }
+internal enum E70 { }
+internal enum E71 { }
+internal enum E72 { }
+internal enum E73 { }
+internal enum E74 { }
+internal enum E75 { }
+internal enum E76 { }
+internal enum E77 { }
+internal enum E78 { }
+internal enum E79 { }
+internal enum E80 { }
+internal enum E81 { }
+internal enum E82 { }
+internal enum E83 { }
+internal enum E84 { }
+internal enum E85 { }
+internal enum E86 { }
+internal enum E87 { }
+internal enum E88 { }
+internal enum E89 { }
+internal enum E90 { }
+internal enum E91 { }
+internal enum E92 { }
+internal enum E93 { }
+internal enum E94 { }
+internal enum E95 { }
+internal enum E96 { }
+internal enum E97 { }
+internal enum E98 { }
+internal enum E99 { }
+internal enum E100 { }
+internal enum E101 { }
+internal enum E102 { }
+internal enum E103 { }
+internal enum E104 { }
+internal enum E105 { }
+internal enum E106 { }
+internal enum E107 { }
+internal enum E108 { }
+internal enum E109 { }
+internal enum E110 { }
+internal enum E111 { }
+internal enum E112 { }
+internal enum E113 { }
+internal enum E114 { }
+internal enum E115 { }
+internal enum E116 { }
+internal enum E117 { }
+internal enum E118 { }
+internal enum E119 { }
+internal enum E120 { }
+internal enum E121 { }
+internal enum E122 { }
+internal enum E123 { }
+internal enum E124 { }
+internal enum E125 { }
+internal enum E126 { }
+internal enum E127 { }
+internal enum E128 { }
+internal enum E129 { }
+internal enum E130 { }
+internal enum E131 { }
+internal enum E132 { }
+internal enum E133 { }
+internal enum E134 { }
+internal enum E135 { }
+internal enum E136 { }
+internal enum E137 { }
+internal enum E138 { }
+internal enum E139 { }
+internal enum E140 { }
+internal enum E141 { }
+internal enum E142 { }
+internal enum E143 { }
+internal enum E144 { }
+internal enum E145 { }
+internal enum E146 { }
+internal enum E147 { }
+internal enum E148 { }
+internal enum E149 { }
+internal enum E150 { }
+internal enum E151 { }
+internal enum E152 { }
+internal enum E153 { }
+internal enum E154 { }
+internal enum E155 { }
+internal enum E156 { }
+internal enum E157 { }
+internal enum E158 { }
+internal enum E159 { }
+internal enum E160 { }
+internal enum E161 { }
+internal enum E162 { }
+internal enum E163 { }
+internal enum E164 { }
+internal enum E165 { }
+internal enum E166 { }
+internal enum E167 { }
+internal enum E168 { }
+internal enum E169 { }
+internal enum E170 { }
+internal enum E171 { }
+internal enum E172 { }
+internal enum E173 { }
+internal enum E174 { }
+internal enum E175 { }
+internal enum E176 { }
+internal enum E177 { }
+internal enum E178 { }
+internal enum E179 { }
+internal enum E180 { }
+internal enum E181 { }
+internal enum E182 { }
+internal enum E183 { }
+internal enum E184 { }
+internal enum E185 { }
+internal enum E186 { }
+internal enum E187 { }
+internal enum E188 { }
+internal enum E189 { }
+internal enum E190 { }
+internal enum E191 { }
+internal enum E192 { }
+internal enum E193 { }
+internal enum E194 { }
+internal enum E195 { }
+internal enum E196 { }
+internal enum E197 { }
+internal enum E198 { }
+internal enum E199 { }
+internal enum E200 { }
+internal enum E201 { }
+internal enum E202 { }
+internal enum E203 { }
+internal enum E204 { }
+internal enum E205 { }
+internal enum E206 { }
+internal enum E207 { }
+internal enum E208 { }
+internal enum E209 { }
+internal enum E210 { }
+internal enum E211 { }
+internal enum E212 { }
+internal enum E213 { }
+internal enum E214 { }
+internal enum E215 { }
+internal enum E216 { }
+internal enum E217 { }
+internal enum E218 { }
+internal enum E219 { }
+internal enum E220 { }
+internal enum E221 { }
+internal enum E222 { }
+internal enum E223 { }
+internal enum E224 { }
+internal enum E225 { }
+internal enum E226 { }
+internal enum E227 { }
+internal enum E228 { }
+internal enum E229 { }
+internal enum E230 { }
+internal enum E231 { }
+internal enum E232 { }
+internal enum E233 { }
+internal enum E234 { }
+internal enum E235 { }
+internal enum E236 { }
+internal enum E237 { }
+internal enum E238 { }
+internal enum E239 { }
+internal enum E240 { }
+internal enum E241 { }
+internal enum E242 { }
+internal enum E243 { }
+internal enum E244 { }
+internal enum E245 { }
+internal enum E246 { }
+internal enum E247 { }
+internal enum E248 { }
+internal enum E249 { }
+internal enum E250 { }
+internal enum E251 { }
+internal enum E252 { }
+internal enum E253 { }
+internal enum E254 { }
+internal enum E255 { }
+internal enum E256 { }
+
+[dotVariant.Variant]
+partial class Variant_big
+{
+    static partial void VariantOf(
+        E1 value1,
+        E2 value2,
+        E3 value3,
+        E4 value4,
+        E5 value5,
+        E6 value6,
+        E7 value7,
+        E8 value8,
+        E9 value9,
+        E10 value10,
+        E11 value11,
+        E12 value12,
+        E13 value13,
+        E14 value14,
+        E15 value15,
+        E16 value16,
+        E17 value17,
+        E18 value18,
+        E19 value19,
+        E20 value20,
+        E21 value21,
+        E22 value22,
+        E23 value23,
+        E24 value24,
+        E25 value25,
+        E26 value26,
+        E27 value27,
+        E28 value28,
+        E29 value29,
+        E30 value30,
+        E31 value31,
+        E32 value32,
+        E33 value33,
+        E34 value34,
+        E35 value35,
+        E36 value36,
+        E37 value37,
+        E38 value38,
+        E39 value39,
+        E40 value40,
+        E41 value41,
+        E42 value42,
+        E43 value43,
+        E44 value44,
+        E45 value45,
+        E46 value46,
+        E47 value47,
+        E48 value48,
+        E49 value49,
+        E50 value50,
+        E51 value51,
+        E52 value52,
+        E53 value53,
+        E54 value54,
+        E55 value55,
+        E56 value56,
+        E57 value57,
+        E58 value58,
+        E59 value59,
+        E60 value60,
+        E61 value61,
+        E62 value62,
+        E63 value63,
+        E64 value64,
+        E65 value65,
+        E66 value66,
+        E67 value67,
+        E68 value68,
+        E69 value69,
+        E70 value70,
+        E71 value71,
+        E72 value72,
+        E73 value73,
+        E74 value74,
+        E75 value75,
+        E76 value76,
+        E77 value77,
+        E78 value78,
+        E79 value79,
+        E80 value80,
+        E81 value81,
+        E82 value82,
+        E83 value83,
+        E84 value84,
+        E85 value85,
+        E86 value86,
+        E87 value87,
+        E88 value88,
+        E89 value89,
+        E90 value90,
+        E91 value91,
+        E92 value92,
+        E93 value93,
+        E94 value94,
+        E95 value95,
+        E96 value96,
+        E97 value97,
+        E98 value98,
+        E99 value99,
+        E100 value100,
+        E101 value101,
+        E102 value102,
+        E103 value103,
+        E104 value104,
+        E105 value105,
+        E106 value106,
+        E107 value107,
+        E108 value108,
+        E109 value109,
+        E110 value110,
+        E111 value111,
+        E112 value112,
+        E113 value113,
+        E114 value114,
+        E115 value115,
+        E116 value116,
+        E117 value117,
+        E118 value118,
+        E119 value119,
+        E120 value120,
+        E121 value121,
+        E122 value122,
+        E123 value123,
+        E124 value124,
+        E125 value125,
+        E126 value126,
+        E127 value127,
+        E128 value128,
+        E129 value129,
+        E130 value130,
+        E131 value131,
+        E132 value132,
+        E133 value133,
+        E134 value134,
+        E135 value135,
+        E136 value136,
+        E137 value137,
+        E138 value138,
+        E139 value139,
+        E140 value140,
+        E141 value141,
+        E142 value142,
+        E143 value143,
+        E144 value144,
+        E145 value145,
+        E146 value146,
+        E147 value147,
+        E148 value148,
+        E149 value149,
+        E150 value150,
+        E151 value151,
+        E152 value152,
+        E153 value153,
+        E154 value154,
+        E155 value155,
+        E156 value156,
+        E157 value157,
+        E158 value158,
+        E159 value159,
+        E160 value160,
+        E161 value161,
+        E162 value162,
+        E163 value163,
+        E164 value164,
+        E165 value165,
+        E166 value166,
+        E167 value167,
+        E168 value168,
+        E169 value169,
+        E170 value170,
+        E171 value171,
+        E172 value172,
+        E173 value173,
+        E174 value174,
+        E175 value175,
+        E176 value176,
+        E177 value177,
+        E178 value178,
+        E179 value179,
+        E180 value180,
+        E181 value181,
+        E182 value182,
+        E183 value183,
+        E184 value184,
+        E185 value185,
+        E186 value186,
+        E187 value187,
+        E188 value188,
+        E189 value189,
+        E190 value190,
+        E191 value191,
+        E192 value192,
+        E193 value193,
+        E194 value194,
+        E195 value195,
+        E196 value196,
+        E197 value197,
+        E198 value198,
+        E199 value199,
+        E200 value200,
+        E201 value201,
+        E202 value202,
+        E203 value203,
+        E204 value204,
+        E205 value205,
+        E206 value206,
+        E207 value207,
+        E208 value208,
+        E209 value209,
+        E210 value210,
+        E211 value211,
+        E212 value212,
+        E213 value213,
+        E214 value214,
+        E215 value215,
+        E216 value216,
+        E217 value217,
+        E218 value218,
+        E219 value219,
+        E220 value220,
+        E221 value221,
+        E222 value222,
+        E223 value223,
+        E224 value224,
+        E225 value225,
+        E226 value226,
+        E227 value227,
+        E228 value228,
+        E229 value229,
+        E230 value230,
+        E231 value231,
+        E232 value232,
+        E233 value233,
+        E234 value234,
+        E235 value235,
+        E236 value236,
+        E237 value237,
+        E238 value238,
+        E239 value239,
+        E240 value240,
+        E241 value241,
+        E242 value242,
+        E243 value243,
+        E244 value244,
+        E245 value245,
+        E246 value246,
+        E247 value247,
+        E248 value248,
+        E249 value249,
+        E250 value250,
+        E251 value251,
+        E252 value252,
+        E253 value253,
+        E254 value254,
+        E255 value255);
+}
+
+[dotVariant.Variant]
+partial class Variant_chonky
+{
+    static partial void VariantOf( // expected-error:25 dotVariant.NotTooManyOptions
+        E1 value1,
+        E2 value2,
+        E3 value3,
+        E4 value4,
+        E5 value5,
+        E6 value6,
+        E7 value7,
+        E8 value8,
+        E9 value9,
+        E10 value10,
+        E11 value11,
+        E12 value12,
+        E13 value13,
+        E14 value14,
+        E15 value15,
+        E16 value16,
+        E17 value17,
+        E18 value18,
+        E19 value19,
+        E20 value20,
+        E21 value21,
+        E22 value22,
+        E23 value23,
+        E24 value24,
+        E25 value25,
+        E26 value26,
+        E27 value27,
+        E28 value28,
+        E29 value29,
+        E30 value30,
+        E31 value31,
+        E32 value32,
+        E33 value33,
+        E34 value34,
+        E35 value35,
+        E36 value36,
+        E37 value37,
+        E38 value38,
+        E39 value39,
+        E40 value40,
+        E41 value41,
+        E42 value42,
+        E43 value43,
+        E44 value44,
+        E45 value45,
+        E46 value46,
+        E47 value47,
+        E48 value48,
+        E49 value49,
+        E50 value50,
+        E51 value51,
+        E52 value52,
+        E53 value53,
+        E54 value54,
+        E55 value55,
+        E56 value56,
+        E57 value57,
+        E58 value58,
+        E59 value59,
+        E60 value60,
+        E61 value61,
+        E62 value62,
+        E63 value63,
+        E64 value64,
+        E65 value65,
+        E66 value66,
+        E67 value67,
+        E68 value68,
+        E69 value69,
+        E70 value70,
+        E71 value71,
+        E72 value72,
+        E73 value73,
+        E74 value74,
+        E75 value75,
+        E76 value76,
+        E77 value77,
+        E78 value78,
+        E79 value79,
+        E80 value80,
+        E81 value81,
+        E82 value82,
+        E83 value83,
+        E84 value84,
+        E85 value85,
+        E86 value86,
+        E87 value87,
+        E88 value88,
+        E89 value89,
+        E90 value90,
+        E91 value91,
+        E92 value92,
+        E93 value93,
+        E94 value94,
+        E95 value95,
+        E96 value96,
+        E97 value97,
+        E98 value98,
+        E99 value99,
+        E100 value100,
+        E101 value101,
+        E102 value102,
+        E103 value103,
+        E104 value104,
+        E105 value105,
+        E106 value106,
+        E107 value107,
+        E108 value108,
+        E109 value109,
+        E110 value110,
+        E111 value111,
+        E112 value112,
+        E113 value113,
+        E114 value114,
+        E115 value115,
+        E116 value116,
+        E117 value117,
+        E118 value118,
+        E119 value119,
+        E120 value120,
+        E121 value121,
+        E122 value122,
+        E123 value123,
+        E124 value124,
+        E125 value125,
+        E126 value126,
+        E127 value127,
+        E128 value128,
+        E129 value129,
+        E130 value130,
+        E131 value131,
+        E132 value132,
+        E133 value133,
+        E134 value134,
+        E135 value135,
+        E136 value136,
+        E137 value137,
+        E138 value138,
+        E139 value139,
+        E140 value140,
+        E141 value141,
+        E142 value142,
+        E143 value143,
+        E144 value144,
+        E145 value145,
+        E146 value146,
+        E147 value147,
+        E148 value148,
+        E149 value149,
+        E150 value150,
+        E151 value151,
+        E152 value152,
+        E153 value153,
+        E154 value154,
+        E155 value155,
+        E156 value156,
+        E157 value157,
+        E158 value158,
+        E159 value159,
+        E160 value160,
+        E161 value161,
+        E162 value162,
+        E163 value163,
+        E164 value164,
+        E165 value165,
+        E166 value166,
+        E167 value167,
+        E168 value168,
+        E169 value169,
+        E170 value170,
+        E171 value171,
+        E172 value172,
+        E173 value173,
+        E174 value174,
+        E175 value175,
+        E176 value176,
+        E177 value177,
+        E178 value178,
+        E179 value179,
+        E180 value180,
+        E181 value181,
+        E182 value182,
+        E183 value183,
+        E184 value184,
+        E185 value185,
+        E186 value186,
+        E187 value187,
+        E188 value188,
+        E189 value189,
+        E190 value190,
+        E191 value191,
+        E192 value192,
+        E193 value193,
+        E194 value194,
+        E195 value195,
+        E196 value196,
+        E197 value197,
+        E198 value198,
+        E199 value199,
+        E200 value200,
+        E201 value201,
+        E202 value202,
+        E203 value203,
+        E204 value204,
+        E205 value205,
+        E206 value206,
+        E207 value207,
+        E208 value208,
+        E209 value209,
+        E210 value210,
+        E211 value211,
+        E212 value212,
+        E213 value213,
+        E214 value214,
+        E215 value215,
+        E216 value216,
+        E217 value217,
+        E218 value218,
+        E219 value219,
+        E220 value220,
+        E221 value221,
+        E222 value222,
+        E223 value223,
+        E224 value224,
+        E225 value225,
+        E226 value226,
+        E227 value227,
+        E228 value228,
+        E229 value229,
+        E230 value230,
+        E231 value231,
+        E232 value232,
+        E233 value233,
+        E234 value234,
+        E235 value235,
+        E236 value236,
+        E237 value237,
+        E238 value238,
+        E239 value239,
+        E240 value240,
+        E241 value241,
+        E242 value242,
+        E243 value243,
+        E244 value244,
+        E245 value245,
+        E246 value246,
+        E247 value247,
+        E248 value248,
+        E249 value249,
+        E250 value250,
+        E251 value251,
+        E252 value252,
+        E253 value253,
+        E254 value254,
+        E255 value255,
+        E256 value256);
+}
