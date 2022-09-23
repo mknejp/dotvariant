2e338b4d3f6fe185d501520022498de9e186f2a7
jvbsl <jvsbl@gmx.de>
2022-09-23 11:34:12 +0200

    Applied suggestions

--- src/dotVariant.Generator.Test/diagnostics/NoImplicitConversionForInterfaces.cs
+++ src/dotVariant.Generator.Test/diagnostics/NoImplicitConversionForInterfaces.cs
@@ -9,3 +9,9 @@ partial class Variant1
 {
     static partial void VariantOf(int a, System.Collections.IEnumerable b); // expected-warning:73 dotVariant.NoImplicitConversionForInterfaces
 }
+
+[dotVariant.Variant]
+partial class Variant2
+{
+    static partial void VariantOf(int a, [dotVariant.NoImplicitConversion] System.Collections.IEnumerable b);
+}
