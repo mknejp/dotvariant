e453d22963ce3d7b0d40b991d7fc70f4042b4ae2
ProphetLamb <prophet.lamb@gmail.com>
2023-06-21 14:02:45 +0200

    Collect diagnostics into array

--- src/dotVariant.Generator/VariantDecl.cs
+++ src/dotVariant.Generator/VariantDecl.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 // Copyright Miro Knejp 2021.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
@@ -6,12 +6,12 @@
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace dotVariant.Generator;
 
 public readonly record struct VariantDecl(INamedTypeSymbol Symbol, TypeDeclarationSyntax Syntax,
-    NullableContext Nullable, IEnumerable<Diagnostic> Diags)
+    NullableContext Nullable, ImmutableArray<Diagnostic> Diags)
 {
     public const string AttributeName = nameof(dotVariant) + "." + nameof(VariantAttribute);
 }
